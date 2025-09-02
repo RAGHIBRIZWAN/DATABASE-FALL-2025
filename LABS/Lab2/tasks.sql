a9807621a11b5f561dd4374fedfc89306e0f00dc
Raghib Rizwan Rabani <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2025-09-02 19:39:58 +0500

    Update tasks.sql

--- LABS/Lab2/tasks.sql
+++ LABS/Lab2/tasks.sql
@@ -20,3 +20,6 @@ select next_day('20-AUG-2022','MONDAY') from dual;
 select to_char(to_date('25-DEC-2023'),'MM-YYYY') from dual;
 select distinct salary from employees order by salary asc;
 select first_name,salary,round(salary,-2) as rounded from employees;
+select department_id from employees group by department_id having count(employee_id) = ( select max(count(employee_id)) from employees group by department_id);
+select department_id, sum(salary) as total_salary_expense from employees group by department_id order by total_salary_expense desc;
+select department_id from employees group by department_id having count(employee_id) = ( select max(count(employee_id)) from employees group by department_id);
